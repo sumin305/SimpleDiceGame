142d21e6758c93c995ae0eaf1a0503a08ae677f3
sumin305 <gogotnals@naver.com>
2023-03-14 22:16:01 +0900

    simple dice game

--- SimpleDiceGame/ViewController.swift
+++ SimpleDiceGame/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var secondImageView: UIImageView!
+    
+    var diceArray: [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
+    
+    func getRandomImage() -> UIImage {
+        return diceArray.randomElement()!
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+        
+        firstImageView.image = getRandomImage()
+        secondImageView.image = getRandomImage()
+    }
+    
 }
 

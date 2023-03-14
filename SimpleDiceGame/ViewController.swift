1b91cc75cf4c00e41d80ccf6d096e5e43a526427
sumin305 <gogotnals@naver.com>
2023-03-14 21:37:42 +0900

    Initial Commit

---
+++ SimpleDiceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleDiceGame
+//
+//  Created by 이수민 on 2023/03/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
